0ad2978dcffa54be690408b41829da0bc7ff8ee6
Anas Abuobaid <abuobaid.anas1999@gmail.com>
2022-09-29 10:54:51 +0300

    edit2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import React, { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
-import { CreateContainer, Header, MainContainer } from "./components";
-import { useStateValue } from "./context/StateProvider";
-import { getAllFoodItems } from "./utils/firebaseFunctions";
-import { actionType } from "./context/reducer";
-import Signin from './components/Signin/Signin'
-import Signup from './components/Signup/Signup'
-import About from './components/About/About';
-import Contact from './components/Contact/Contact';
-
-
+import React, { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import { CreateContainer, Header, MainContainer } from './components';
+import { useStateValue } from './context/StateProvider';
+import { getAllFoodItems } from './utils/firebaseFunctions';
+import { actionType } from './context/reducer';
 
 const App = () => {
   const [{ foodItems }, dispatch] = useStateValue();
@@ -30,17 +24,13 @@ const App = () => {
 
   return (
     <AnimatePresence exitBeforeEnter>
-      <div className="w-screen h-auto flex flex-col bg-primary">
+      <div className='w-screen h-auto flex flex-col bg-primary'>
         <Header />
 
-        <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
+        <main className='mt-14 md:mt-20 px-4 md:px-16 py-4 w-full'>
           <Routes>
-            <Route path="/signin " element={<Signin />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/*" element={<MainContainer />} />
-            <Route path="/createItem" element={<CreateContainer />} />
+            <Route path='/*' element={<MainContainer />} />
+            <Route path='/createItem' element={<CreateContainer />} />
           </Routes>
         </main>
       </div>
