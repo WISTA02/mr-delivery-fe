16899b0e8fbfc530721bc068d8c780432313f28f
ft7e <ibraheemhamodeh@gmail.com>
2022-10-03 11:51:19 +0300

    edited extra footer

--- src/App.js
+++ src/App.js
@@ -10,17 +10,17 @@ import Signin from './components/Signin/Signin.jsx';
 import UserProfile from './components/Profile/UserProfile';
 import EditProfile from './components/Profile/EditProfile';
 import About from './components/About/about';
-import Home2 from "./pages/home/Home";
-import UserList from "./pages/userList/UserList";
-import User from "./pages/user/User";
-import NewUser from "./pages/newUser/NewUser";
-import ProductList from "./pages/productList/ProductList";
-import Product from "./pages/product/Product";
-import NewProduct from "./pages/newProduct/NewProduct";
-import WidgetLg from "./components/widgetLg-driver/WidgetLg";
-import WidgetLg2 from "./components/widgetLg-rest/WidgetLg";
-import Footer from './components/Footer/Footer'
-import Contact from "./components/Contact/Contact";
+import Home2 from './pages/home/Home';
+import UserList from './pages/userList/UserList';
+import User from './pages/user/User';
+import NewUser from './pages/newUser/NewUser';
+import ProductList from './pages/productList/ProductList';
+import Product from './pages/product/Product';
+import NewProduct from './pages/newProduct/NewProduct';
+import WidgetLg from './components/widgetLg-driver/WidgetLg';
+import WidgetLg2 from './components/widgetLg-rest/WidgetLg';
+import Footer from './components/Footer/Footer';
+import Contact from './components/Contact/Contact';
 import Restaurant from './components/restaurantPage/restaurant';
 
 const App = () => {
@@ -47,18 +47,18 @@ const App = () => {
         {/* <Sidebar /> */}
         <main className='mt-14 md:mt-20 px-4 md:px-16 py-4 w-full x'>
           <Routes>
-            <Route path="/*" element={<MainContainer />} />
-            <Route path="/createItem" element={<CreateContainer />} />
-            <Route path="/admin" element={<Home2 />}></Route>
-            <Route path="/admin/driver" element={<WidgetLg />}></Route>
-            <Route path="/admin/resturant" element={<WidgetLg2 />}></Route>
-            <Route path="/Ordersapprove" element={<UserList />}></Route>
-            <Route path="/user/:userId" element={<User />}></Route>
-            <Route path="/newUser" element={<NewUser />}></Route>
-            <Route path="/driver-order" element={<ProductList />}></Route>
-            <Route path="/product/:productId" element={<Product />}></Route>
-            <Route path="/newproduct" element={<NewProduct />}></Route>
-            <Route path="/contactus" element={<Contact />} />
+            <Route path='/*' element={<MainContainer />} />
+            <Route path='/createItem' element={<CreateContainer />} />
+            <Route path='/admin' element={<Home2 />}></Route>
+            <Route path='/admin/driver' element={<WidgetLg />}></Route>
+            <Route path='/admin/resturant' element={<WidgetLg2 />}></Route>
+            <Route path='/Ordersapprove' element={<UserList />}></Route>
+            <Route path='/user/:userId' element={<User />}></Route>
+            <Route path='/newUser' element={<NewUser />}></Route>
+            <Route path='/driver-order' element={<ProductList />}></Route>
+            <Route path='/product/:productId' element={<Product />}></Route>
+            <Route path='/newproduct' element={<NewProduct />}></Route>
+            <Route path='/contactus' element={<Contact />} />
             <Route path='/about' element={<About />} />
             <Route path='/signup' element={<Signup />} />
             <Route path='/signin' element={<Signin />} />
@@ -66,7 +66,6 @@ const App = () => {
             <Route path='/editprofile' element={<EditProfile />} />
             <Route path='/restaurant' element={<Restaurant />} />
           </Routes>
-          <Footer />
         </main>
         <Footer />
       </div>
