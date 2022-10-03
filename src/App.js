5529d4fbb11acd6f36252c112ba204293e7889f4
engtasneemmaq <tasneemmaq@yahoo.com>
2022-10-03 12:55:01 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ const App = () => {
             <Route path='/editprofile' element={<EditProfile />} />
             <Route path='/restaurant' element={<Restaurant />} />
           </Routes>
-          <Footer />
+         
         </main>
         <Footer />
       </div>
--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -4,152 +4,183 @@ import samah from '../../img/samah.jpg'
 import anas from '../../img/anas.jpg'
 import ibraheem from '../../img/ibraheem.jpg'
 import wajeeh from '../../img/wajeeh.jpg'
-
+import './about.css'
 function About(){
 
   return (
-  <div>
-<br/>
-<h1 className="font-sans hover:font-serif text-grey-900 text-4xl font-xl font-extrabold text-center ">Project developers -Authors ​​​</h1>
-<br/>
-<p className='mb-4 text-xl tracking-tight font-bold text-center text-yellow-500  dark:text-white hover:shadow-md cursor-pointer'>Now we can say about ourselves
-Full stack developer
-After a long and intense six months with the ASAC, it was filled with many information and tasks.
-We were able to do this project, we are proud of ourselves, during this simple period, we learned many skills.
-<br/>We hope that you will like it and support you.
-WISTA TEAM</p>
-<div className='main 1 flex '  >
+<div>
   <br/>
-<div className="max-w-full rounded overflow-hidden shadow-lg m-6 w-80 mr-56 ">
-  <img className="w-sm" src={tasneem} alt="Sunset in the mountains"/>
-  <div className="px-4 py-4">
-    <div className="font-bold text-sm mb-2">Tasneem Ali Maqableh</div>
-    <p className="text-yellow-500 text-sm font-medium ">
-FullStack web developer - Telecomm and software engineer  </p>
+  <br/>
+<h1 className="font-sans  text-grey-900 text-4xl font-xl font-bold text-center ">Project Team Member ​​​</h1>
 <br/>
-         <p className="text-yellow-500 text-sm font-medium "> Amman- Jordan,</p>
+<p className='mb-4 text-xl tracking-tight  text-center text-black  dark:text-white hover:shadow-md cursor-pointer'>
+  Now we can say about ourselves
+Full stack developer<br/>
+After a long and intense six months with the ASAC, it was filled with many information and tasks.<br/>
+We were able to do this project, we are proud of ourselves, during this simple period, we learned many skills.
+We hope that you will like it and support you.
+WISTA TEAM</p>
+
+    <div class="container">
+      
+  <div class="card">
+  <br/>
+    <img src={tasneem} alt="Person" class="card__image"/>
+    <div className="px-4 py-4"></div>
+    <div className="font-bold text-sm mb-2">Tasneem Maqableh</div>
+    <p className="text-black text-sm font-medium ">
+FullStack web developer -<br/> Telecom and software engineer  </p>
+<br/>
+         <p className="text-black text-sm font-medium "> Amman- Jordan,</p>
           <br />
-          <p className="text-yellow-500 text-sm font-medium ">​​📞​ 00962792934412</p>
+          <p className="text-black text-sm font-medium ">​​📞​ 00962792934412</p>
           <br/>
-          <span className="text-yellow-500 text-sm font-small ">​📧​tasneemmaq89@gmail.com</span>
-  </div>
+          <span className="text-black text-sm font-small ">​📧 ​tasneemmaq89@gmail.com</span>
+
   <div className="px-6 pt-4 pb-2">
     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-900 mr-2 mb-2">
-    <a href="https://github.com/engTasneemmaq" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/github/github-icon.svg" alt="git" width="20" height="20"/> </a>
+    <a href="  https://github.com/engTasneemmaq" class="text-gray-600">
+        <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github"
+          class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+          <path fill="currentColor"
+            d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z">
+          </path>
+        </svg>
+      </a>
 </span>
     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-900 mr-2 mb-2">
     <a href="https://www.linkedin.com/in/tasneem-maqableh/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/linkedin/linkedin-icon.svg" alt="git" width="20" height="20"/> </a>
 </span>
   </div>
-</div>
-
-<div className="max-w-full rounded overflow-hidden shadow-lg m-9 w-80 mr-56">
-  <img className="w-sm" src={samah} alt="Sunset in the mountains"/>
-  <div className="px-4 py-4">
-    <div className="font-bold text-sm mb-2">Samah Al satary</div>
-    <p className="text-yellow-500 text-sm font-medium ">
-FullStack web developer - Computer engineer  </p>
-<br/>
-         <p className="text-yellow-500 text-sm font-medium "> Amman- Jordan,</p>
-          <br />
-          <p className="text-yellow-500 text-sm font-medium ">​​📞​ 00962790548262</p>
-          <br/>
-          <span className="text-yellow-500 text-sm font-small ">​📧​ alsatary.samah@gmail.com</span>
   </div>
+
+  <div class="card">
+    <br/>
+    <img src={samah} alt="Person" class="card__image"/>
+    <div className="px-4 py-4"></div>
+    <div className="font-bold text-sm mb-2">Samah Al satary</div>
+    <p className="text-black text-sm font-medium ">
+    FullStack web developer - Computer engineer  </p>
+<br/>
+         <p className="text-black text-sm font-medium "> Amman- Jordan,</p>
+          <br />
+          <p className="text-black text-sm font-medium ">​​📞​ 00962790548262</p>
+          <br/>
+          <span className="text-black text-sm font-small ">​📧​ alsatary.samah@gmail.com</span>
+
   <div className="px-6 pt-4 pb-2">
     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-900 mr-2 mb-2">
-    <a href="https://github.com/alsatarysamah" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/github/github-icon.svg" alt="git" width="20" height="20"/> </a>
+    <a href=" https://github.com/alsatarysamah" class="text-gray-600">
+        <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github"
+          class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+          <path fill="currentColor"
+            d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z">
+          </path>
+        </svg>
+      </a>
 </span>
     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-900 mr-2 mb-2">
     <a href="https://www.linkedin.com/in/samah-alsatary/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/linkedin/linkedin-icon.svg" alt="git" width="20" height="20"/> </a>
 </span>
   </div>
-</div>
-
-<div className="max-w-full rounded overflow-hidden shadow-lg m-6 w-80 ">
-  <img className="w-sm" src={anas} alt="Sunset in the mountains"/>
-  <div className="px-4 py-4 ">
+ 
+  </div>
+  <div class="card">
+    <br/>
+    <img src={anas} alt="Person" class="card__image"/>
+    <div className="px-4 py-4"></div>
     <div className="font-bold text-sm mb-2">Anas abuobaid</div>
-    <p className="text-yellow-500 text-sm font-medium ">
+    <p className="text-black text-sm font-medium ">
 FullStack web developer - Computer Science  </p>
 <br/>
-         <p className="text-yellow-500 text-sm font-medium "> Amman- Jordan,</p>
+         <p className="text-black text-sm font-medium "> Amman- Jordan,</p>
           <br />
-          <p className="text-yellow-500 text-sm font-medium ">​​📞​ 00962787810864</p>
+          <p className="text-black text-sm font-medium ">​​📞​ 00962787810864</p>
           <br/>
-          <span className="text-yellow-500 text-sm font-small ">​📧​ abuobaid.anas1999@gmail.com</span>
-  </div>
-
+          <span className="text-black text-sm font-small ">​📧​ abuobaid.anas1999@gmail.com</span>
 
   <div className="px-6 pt-4 pb-2">
     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-900 mr-2 mb-2">
-    <a href="https://github.com/abuobaid9" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/github/github-icon.svg" alt="git" width="20" height="20"/> </a>
+    <a href="  https://github.com/abuobaid9" class="text-gray-600">
+        <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github"
+          class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+          <path fill="currentColor"
+            d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z">
+          </path>
+        </svg>
+      </a>
 </span>
     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-900 mr-2 mb-2">
     <a href="https://www.linkedin.com/in/anas-abuobaid/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/linkedin/linkedin-icon.svg" alt="git" width="20" height="20"/> </a>
 </span>
   </div>
-</div>
-
-
-</div>
-
-<div className='main 2  flex '>
-<div className="max-w-full rounded overflow-hidden shadow-lg m-9 w-80 mr-56">
-  <img className="w-sm" src={ibraheem} alt="Sunset in the mountains"/>
-  <div className="px-4 py-4">
-    <div className="font-bold text-sm mb-2">Ibraheem Hammoudeh</div>
-    <p className="text-yellow-500 text-sm font-medium ">
-FullStack web developer - Software Engineer  </p>
-<br/>
-         <p className="text-yellow-500 text-sm font-medium "> Amman- Jordan,</p>
-          <br />
-          <p className="text-yellow-500 text-sm font-medium ">​​📞​ 00962782524069</p>
-          <br/>
-          <span className="text-yellow-500 text-sm font-medium ">​📧​ ibraheemhamodeh@gmail.com</span>
   </div>
+
+  <div class="card">
+    <img src={ibraheem} alt="Person" class="card__image"/>
+    <div className="px-4 py-4"></div>
+    <div className="font-bold text-sm mb-2">Ibraheem Hammoudeh</div>
+    <p className="text-black text-sm font-medium ">
+    FullStack web developer - Software Engineer  </p>
+<br/>
+         <p className="text-black text-sm font-medium "> Amman- Jordan,</p>
+          <br />
+          <p className="text-black text-sm font-medium ">📞​ 00962782524069</p>
+          <br/>
+          <span className="text-black text-sm font-small ">​📧​ ibraheemhamodeh@gmail.com</span>
+
   <div className="px-6 pt-4 pb-2">
     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-900 mr-2 mb-2">
-    <a href="https://github.com/ft7e" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/github/github-icon.svg" alt="git" width="20" height="20"/> </a>
+    <a href="https://github.com/ft7e" class="text-gray-600">
+        <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github"
+          class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+          <path fill="currentColor"
+            d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z">
+          </path>
+        </svg>
+      </a>
 </span>
     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-900 mr-2 mb-2">
     <a href="https://www.linkedin.com/in/ibraheem-hammoudeh/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/linkedin/linkedin-icon.svg" alt="git" width="20" height="20"/> </a>
 </span>
   </div>
-</div>
-
-<div className="max-w-full rounded overflow-hidden shadow-lg m-9  w-80 mr-56">
-  <img className="w-sm" src={wajeeh} alt="Sunset in the mountains"/>
-  <div className="px-4 py-4">
-    <div className="font-bold text-sm mb-2">Wajeeh Hussein</div>
-    <p className="text-yellow-500 text-sm font-medium ">
-FullStack web developer - Network Engineer   </p>
-<br/>
-         <p className="text-yellow-500 text-sm font-medium "> Amman- Jordan,</p>
-          <br />
-          <p className="text-yellow-500 text-sm font-medium ">​​📞​ 00962780910958</p>
-          <br/>
-          <span className="text-yellow-500 text-sm font-medium ">​📧​ @gmail.com</span>
   </div>
+
+  <div class="card">
+    <img src={wajeeh} alt="Person" class="card__image"/>
+    <div className="px-4 py-4"></div>
+    <div className="font-bold text-sm mb-2">Wajeeh Hussein</div>
+    <p className="text-black text-sm font-medium ">
+    FullStack web developer - Network Engineer  </p>
+<br/>
+         <p className="text-black text-sm font-medium "> Amman- Jordan,</p>
+          <br />
+          <p className="text-black text-sm font-medium ">​​​​📞​ 00962780910958</p>
+          <br/>
+          <span className="text-black text-sm font-small ">​📧​ wajehessam3@gmail.com</span>
+
   <div className="px-6 pt-4 pb-2">
     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-900 mr-2 mb-2">
-    <a href="https://github.com/WajeehHussein" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/github/github-icon.svg" alt="git" width="20" height="20"/> </a>
-</span>
+    <a href="https://github.com/WajeehHussein" class="text-gray-600">
+        <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github"
+          class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+          <path fill="currentColor"
+            d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z">
+          </path>
+        </svg>
+      </a></span>
     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-900 mr-2 mb-2">
-    <a href="" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/linkedin/linkedin-icon.svg" alt="git" width="20" height="20"/> </a>
+    <a href="https://www.linkedin.com/in/wajeeh-essam/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/linkedin/linkedin-icon.svg" alt="git" width="20" height="20"/> </a>
 </span>
   </div>
-</div>
-
-
-  
-</div>
 
   </div>
   
+</div>
 
-    
+</div>
+
   )
 }
 
-export default About
+export default About;
