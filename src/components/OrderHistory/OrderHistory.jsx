cddf3075062660eb747d53385136a583599e7c77
samah anwer alsatary <alsatary.samah@yahoo.com>
2022-10-11 09:31:02 +0300

    datagrid and popup

--- src/components/OrderHistory/OrderHistory.jsx
+++ src/components/OrderHistory/OrderHistory.jsx
@@ -4,20 +4,26 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import { useEffect } from "react";
+// import Box from '@mui/material/Box';
 import axios from "axios";
 import Cookies from "universal-cookie";
+
+
 const cookies = new Cookies();
 
 export default function OrderHistory() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const [data, setData] = useState([]);
   const [statusOrder, setStatusOrder] = useState("");
- 
+  const [id, setID] = useState();
 
   const handleChange = (id) => {
-    console.log({id});
+    console.log("*************", id);
+    setID(id);
     const config = {
       headers: { Authorization: `Bearer ${cookies.get("data").user.token}` },
-
     };
     // console.log(`https://mr-delivery-wista.herokuapp.com/order/${id}`);
     axios
@@ -26,7 +32,25 @@ export default function OrderHistory() {
         console.log(data.data.status);
         let x = data.data.status;
         setStatusOrder(x);
-        console.log({statusOrder});
+        console.log({ statusOrder });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  const handleChange2 = (id) => {
+    console.log("222222222 ", id);
+    const config = {
+      headers: { Authorization: `Bearer ${cookies.get("data").user.token}` },
+    };
+    // console.log(`https://mr-delivery-wista.herokuapp.com/order/${id}`);
+    axios
+      .get(`https://mr-delivery-wista.herokuapp.com/order/${id}`, config)
+      .then((data) => {
+        console.log(data.data.status);
+        let x = data.data.status;
+        setStatusOrder(x);
+        console.log({ statusOrder });
       })
       .catch((e) => {
         console.log(e);
@@ -35,17 +59,20 @@ export default function OrderHistory() {
 
   async function getOrder() {
     try {
-      let  orderDetails = "";
-      const response = await axios.get("https://mr-delivery-wista.herokuapp.com/user-history", {
-        headers: {
-          Authorization: `Bearer ${cookies.get("data").user.token}`,
-        },
-      });
+      let orderDetails = "";
+      const response = await axios.get(
+        "https://mr-delivery-wista.herokuapp.com/user-history",
+        {
+          headers: {
+            Authorization: `Bearer ${cookies.get("data").user.token}`,
+          },
+        }
+      );
       response.data.map((element) => {
         element.all_items.forEach((e) => {
-          orderDetails += e.quantity+ " " + e.name + ",";
+          orderDetails += e.quantity + " " + e.name + ",";
         });
-        element.all_items = orderDetails.slice(0, -1);;
+        element.all_items = orderDetails.slice(0, -1);
         orderDetails = "";
       });
       setData(response.data);
@@ -53,18 +80,17 @@ export default function OrderHistory() {
     } catch (err) {
       console.log(err);
     }
-  } 
+  }
   useEffect(() => {
     getOrder();
   }, []);
   useEffect(() => {
-    getOrder();
+    // getOrder();
   }, [statusOrder]);
   const columns = [
-    { field: "id", headerName: "ID", width: 150 },
- 
-    { field: "all_items", headerName: "Order Details", width: 200 
-  },
+    { field: "id", headerName: "ID", width: 150,sortable: false },
+
+    { field: "all_items", headerName: "Order Details", width: 200,sortable: false },
 
     // {
     //   field: "status",
@@ -75,41 +101,55 @@ export default function OrderHistory() {
       field: "total_price",
       headerName: "Total Price",
       width: 160,
-      
+      sortable: false
     },
-    
-  { field: "order Status", headerName: "Show Status", width: 300 ,
-  renderCell: (params) => {
-    return (
-<>
-<label htmlFor="my-modal-3" className=" modal-button w-3 bg-white-500 hover:bg-yellow-400  text-sm text-yellow-400 " onClick={() => handleChange(params.row.id)}>Show Status</label>
-<input type="checkbox" id="my-modal-3" className="modal-toggle " />
-<div className="modal">
-  <div className="modal-box relative">
 
-  <label htmlFor="my-modal-3" className="bg-black  btn-sm rounded-md p-2 inline-flex items-center justify-center text-white hover:text-black-500 hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-inset ">✕</label>
-              
-    <h3 className="text-lg   bg-yellow-300 text-center">Status order</h3>
-    <p className="py-4 text-lg  text-black text-center"> {statusOrder}</p>
-  <div className="flex-none">
-
-
-  <button  className=" text-gray-800  py-2 px-2  w-40">
-   <Link to="/orderhistory">Refresh</Link>
-   
-  </button>
-
-  </div>
-  </div>
-</div>
+    {
+      field: "order Status",
+      headerName: "Show Status",
+      width: 300,
+      renderCell: (params) => {
+        return (
         
-        
-    
-      </>
-    );
-  },
-},
-    
+          <>
+            <label
+              htmlFor="my-modal-3"
+              className=" modal-button w-3 bg-white-500 hover:bg-yellow-400  text-sm text-yellow-400 "
+              onClick={() => handleChange(params.row.id)}
+            >
+              Show Status
+            </label>
+            <input type="checkbox" id="my-modal-3" className="modal-toggle " />
+            <div className="modal">
+              <div className="modal-box relative">
+                <label
+                  htmlFor="my-modal-3"
+                  className="bg-black  btn-sm rounded-md p-2 inline-flex items-center justify-center text-white hover:text-black-500 hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-inset "
+                >
+                  ✕
+                </label>
+
+                <h3 className="text-lg   bg-yellow-300 text-center">
+                  Status order  {id} 
+                </h3>
+                <p className="py-4 text-lg  text-black text-center">
+                  {" "}
+                   {statusOrder}
+                </p>
+                <div className="flex-none">
+                  <button
+                    className=" text-gray-800  py-2 px-2  w-40"
+                    onClick={() => handleChange(id)}
+                  >
+                    Refresh
+                  </button>
+                </div>
+              </div>
+            </div>
+          </>
+        );
+      },
+    },
   ];
 
   return (
@@ -119,7 +159,7 @@ export default function OrderHistory() {
         <DataGrid
           rows={data}
           disableSelectionOnClick
-          columns={columns}
+          columns={[...columns, { field: 'id', sortable: false }]}
           pageSize={20}
           checkboxSelection
         />
